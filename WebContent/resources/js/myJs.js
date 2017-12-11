518f575b8bdb0ed5bec30d8c43b89865f61c38d7
jlopes <joaolopes71@hotmail.com>
2017-12-11 09:28:56 -0200

    commit inicial: front end quase pronto

---
+++ WebContent/resources/js/myJs.js
@@ -0,0 +1,23 @@
+$( function() {
+	$( ".datepicker" ).datepicker({
+		  showOn: "button",
+	      buttonImage: "http://jqueryui.com/resources/demos/datepicker/images/calendar.gif",
+	      buttonImageOnly: true,
+	      buttonText: "Select date",
+	      changeMonth: true,
+	      changeYear: true,
+	      yearRange: "1960:2017"
+	});
+});
+
+$( function() {
+$('.btnNext').click(function(){
+		$('.nav-tabs > .active').next('li').find('a').trigger('click');
+	});
+});
+
+$( function() {
+	$('.btnPrevious').click(function(){
+		$('.nav-tabs > .active').prev('li').find('a').trigger('click');
+	});
+});
